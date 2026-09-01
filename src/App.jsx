7835ab53dc625874ccf3e4a3ff8722ff1945767a
nikolaipisklakov <kimysol.ceo@gmail.com>
2026-09-01 23:09:06 +0300

    Add lesson 01

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Lesson01 from "./lessons/Lesson01/Lesson01";
+function App() {
+  return (
+    <div className="app">
+      {/* Lesson 01. Introduction */}
+      <Lesson01/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+// Упрощенный иморт стилей
+import "./styles.css"
+function Button(){
+    return <button className="my-button">My button</button>
+}
+// Компоненты экспортируем по умолчанию 
+export default Button
+
